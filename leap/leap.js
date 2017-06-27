1c9b6644f6de6da4f7a0b4a3c04609a46316f52a
Ralph Dugue <rdugue1@gmail.com>
2017-06-27 00:36:51 -0400

    Finished solution for leap

---
+++ leap/leap.js
@@ -0,0 +1,15 @@
+export default class Year {
+    constructor(year) {
+        this.year = year
+    }
+
+    isLeap() {
+        if (this.year % 4 === 0 && this.year % 100 !== 0) {
+            return true
+        } else if (this.year % 100 === 0 && this.year % 400 === 0) {
+            return true
+        }
+
+        return false
+    }
+}
